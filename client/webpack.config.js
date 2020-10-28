0af51384fb3ed6837090f8bc4b70e5bffe036b81
JC Chung <jotace422@gmail.com>
2020-10-29 00:36:27 +0900

    [Build]: eslint & prettier 설정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    name: 'issue-tracker',
-    mode: 'development',
-    resolve: {
-      extensions : ['.jsx', '.js'],
-    },
-    devServer:{
-      contentBase: path.join(__dirname, "dist"),
-      publicPath: "/",
-      host: "localhost",
-      overlay: true,
-      port: 8200,
-      stats: "errors-only",
-    },
-    module:{
-        rules:[{
-            test:/\.jsx?/,
-            use:{
-             loader: 'babel-loader'
-            },
-            exclude: /node_modules/,
-         }],  
-    },
-    entry:{
-      app: ['./src/hello']
-    },
-    output:{
-        path:path.join(__dirname, 'dist'),
-        filename: 'app.js'
-    },
-    plugins: [
-      new Dotenv({
-        path: './.env',
-      }),
-      new HtmlWebpackPlugin({
-        template: 'src/index.html',
-      }), 
-      new CleanWebpackPlugin()
-    ]
-}
+  name: "issue-tracker",
+  mode: "development",
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: "/",
+    host: "localhost",
+    overlay: true,
+    port: 8200,
+    stats: "errors-only",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  entry: {
+    app: ["./src/index"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env",
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
