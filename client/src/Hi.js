5d11bcfe7ab71ae12e289d5ad94bbbf90bb16459
thdwlsgus0 <thdwlsgus0@naver.com>
2020-10-28 21:23:32 +0900

    [build] : 환경 설정
    1. hello.js -> Hi.js로 변경

--- client/src/Hi.js
+++ client/src/Hi.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {Component} from 'react';
 
 const Hi = () =>{
    return ( 
       <h1>반갑습니다.!</h1>
    );
 }
+
 
 ReactDOM.render(
   <Hi />,
