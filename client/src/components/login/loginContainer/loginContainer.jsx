23e024bc0ca2b73aa8ac5c7296e58730912c7e56
mu1616 <mu1616@naver.com>
2020-10-30 06:03:07 +0900

    [style]: 코드 정리, 파일 정리 #2
    들여쓰기, eslint 오류나는 부분의 코드를 정리하였습니다.
    더 이상 쓸모없는 .keep 파일들을 삭제하였습니다.

--- client/src/components/login/loginContainer/loginContainer.jsx
+++ client/src/components/login/loginContainer/loginContainer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import './LoginContainer.scss';
-const LoginContainer = ({children}) =>{
-    return (
-        <>
-        <div className="login-block">
-            <div className="login-title">이슈 트래커</div>
-            {children}
-        </div>
-      </>
-    );
-  
-}
-  
+
+const LoginContainer = ({ children }) => (
+  <>
+    <div className="login-block">
+      <div className="login-title">이슈 트래커</div>
+      {children}
+    </div>
+  </>
+);
+
 export default LoginContainer;
