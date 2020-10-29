c7e62eb0fe992f61adea337524d5bab3c038bfc0
thdwlsgus0 <thdwlsgus0@naver.com>
2020-10-29 15:07:23 +0900

    [build]: 모듈 설치
    1. react-router-dom 설치하였습니다.

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+export default () => {
+ return(
+    <div cladssName="App">App입니다</div>
+ );
+
+}
