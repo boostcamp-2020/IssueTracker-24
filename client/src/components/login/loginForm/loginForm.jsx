71f2ab2b7523595e4fd361a448249641882ab239
thdwlsgus0 <thdwlsgus0@naver.com>
2020-10-29 17:48:27 +0900

    [feat]: github 로그인 버튼 생성
    1. github 로그인 버튼을 생성하였습니다.

--- client/src/components/login/loginForm/loginForm.jsx
+++ client/src/components/login/loginForm/loginForm.jsx
@@ -8,12 +8,15 @@ const LoginFormContainer = ({children}) =>{
        <form className="input-container">
          <div>
             <div className="input-title">아이디</div>
-            <input type="text" className="input-value" value="아이디를 입력하세요"/>
+            <input type="text" className="input-value"/>
             <div className="input-title">패스워드</div>
-            <input type="password" className="input-value" value="비밀번호를 입력하세요"/>
-            <div className="input-common-button">
+            <input type="text" className="input-value"/>
+            <div className="input-common-button button">
               <button>로그인</button>
               <button>회원가입</button>
+            </div>
+            <div className="input-github-button button">
+              <button>Sign in with GitHub</button>
             </div>
          </div>
        </form>
