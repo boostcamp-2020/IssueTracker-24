ec1dfe11c6b211398328884ae002aefa6eb8572e
JC Chung <jotace422@gmail.com>
2020-10-30 09:34:39 +0900

    [Build]: github-strategy, jwt-strategy를 passport/index.js 에서 설정하다록 추가 #2

--- server/src/passport/jwt-strategy.js
+++ server/src/passport/jwt-strategy.js
@@ -8,26 +8,28 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.jwt.secretKey;
 
-passport.use(
-  new JwtStrategy(opts, (jwtPayload, done) => {
-    const findOption = {};
-    if (jwtPayload.snsId) {
-      // github-login
-      findOption.sns_id = jwtPayload.snsId;
-      findOption.provider = jwtPayload.provider;
-    } else {
-      // local-login
-      findOption.user_id = jwtPayload.userId;
-    }
+module.exports = () => {
+  passport.use(
+    new JwtStrategy(opts, (jwtPayload, done) => {
+      const findOption = {};
+      if (jwtPayload.snsId) {
+        // github-login
+        findOption.sns_id = jwtPayload.snsId;
+        findOption.provider = jwtPayload.provider;
+      } else {
+        // local-login
+        findOption.user_id = jwtPayload.userId;
+      }
 
-    User.findOne(findOption, (err, user) => {
-      if (err) {
-        return done(err, false);
-      }
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    });
-  }),
-);
+      User.findOne(findOption, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      });
+    }),
+  );
+};
