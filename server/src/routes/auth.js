c5831e883c78ecfca5f797269a121e504b445128
JC Chung <jotace422@gmail.com>
2020-10-31 23:17:01 +0900

    [feat]: logout 미들웨어 추가 #2
    logout 라우트를 추가하였고, isAuthenticate라는 미들웨어를 중간에 넣어 로그인 된 상태에서만 logout을 시도하도록 추가
    github 로그인 라우트에는 isNotAuthenticated 미들웨어를 넣어서 인증되지 않은, 즉 req.header에 토큰이 없는 사용자만 login을 시도하도록 추가

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,11 +3,14 @@ const passport = require('passport');
 
 const router = express.Router();
 const AuthController = require('../controllers/auth');
+const { isAuthenticated, isNotAuthenticated } = require('../middlewares/auth');
 
-router.get('/github/login', passport.authenticate('github'));
+/* 로그인 되지 않은 상태에서만 login 시도 가능 */
+router.get('/github/login', isNotAuthenticated, passport.authenticate('github'));
+
 router.get('/github/callback', AuthController.githubAuth);
 
-// TODO: logout을 위한 controller 작성 필요
-// router.get('/logout',);
+/* 로그인 된 상태에서만 logout 시도 가능 */
+router.get('/logout', isAuthenticated, AuthController.logout);
 
 module.exports = router;
