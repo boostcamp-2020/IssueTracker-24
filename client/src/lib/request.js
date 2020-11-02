c44ae64befe844780fb8c33d65509b95167038ae
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-02 19:26:59 +0900

    [refactor]: axios 설정 수정 및 코드 수정
    1. baseURL을 default로 설정해서 코드 간소화
    2. putData 함수 생성

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,56 +1,59 @@
 import axios from 'axios';
 import { getToken } from '../utils/token';
 
-const API_URL =
+axios.defaults.baseURL =
   process.env.NODE_ENV === 'production'
     ? `${process.env.PROD_URL}`
     : `${process.env.DEV_URL}`;
 
 const getOptions = (body) => {
-  const headers = { 'Content-Type': 'application/json; charset=UTF-8'};
   const options = {
     mode: 'cors',
     credentials: 'include',
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
   };
-  options.headers = headers;
 
   const token = getToken();
-  if (token) {
-    options.headers = { Authorization: token };
-  }
+  if (token) options.headers = { ...options.headers, Authorization: token };
 
-  if (body) {
-    options.body = JSON.stringify(body);
-  }
+  if (body) options.body = JSON.stringify(body);
 
   return options;
 };
 
-const getData = async (url, body) => {
-  const options = getOptions(body);
-  const response =  await axios.get(`${API_URL}${url}`, options);
-  return response;
+const getData = async (url) => {
+  const options = getOptions();
+
+  const response = await axios.get(url, options);
+  return response.data;
 };
 
 const postData = async (url, body) => {
   const options = getOptions(body);
 
-  const response = await axios.post(`${API_URL}${url}`, options);
-  return response;
+  const response = await axios.post(url, options);
+  return response.data;
 };
 
 const patchData = async (url, body) => {
   const options = getOptions(body);
 
-  const response = await axios.patch(`${API_URL}${url}`, options);
-  return response;
+  const response = await axios.patch(url, options);
+  return response.data;
 };
 
-const deleteData = async (url, body) => {
+const putData = async (url, body) => {
   const options = getOptions(body);
 
-  const response = await axios.delete(`${API_URL}${url}`, options);
-  return response;
+  const response = await axios.put(url, options);
+  return response.data;
 };
 
-export { getData, postData, patchData, deleteData };
+const deleteData = async (url) => {
+  const options = getOptions();
+
+  const response = await axios.delete(url, options);
+  return response.data;
+};
+
+export { getData, postData, patchData, putData, deleteData };
