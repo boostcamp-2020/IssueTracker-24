dc2e587f313dfa925fa7c9983f7b81d8a34eeb83
JC Chung <jotace422@gmail.com>
2020-11-02 15:14:19 +0900

    [fix]: 로그아웃 요청시 isAuth 미들웨어를 거친다 #2

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,14 +3,12 @@ const passport = require('passport');
 
 const router = express.Router();
 const AuthController = require('../controllers/auth');
-const { isAuthenticated, isNotAuthenticated } = require('../middlewares/auth');
+const { isAuth } = require('../middlewares/auth');
 
-/* 로그인 되지 않은 상태에서만 login 시도 가능 */
-router.get('/github/login', isNotAuthenticated, passport.authenticate('github'));
+router.get('/github/login', passport.authenticate('github'));
 
 router.get('/github/callback', AuthController.githubAuth);
 
-/* 로그인 된 상태에서만 logout 시도 가능 */
-router.get('/logout', isAuthenticated, AuthController.logout);
+router.get('/logout', isAuth, AuthController.logout);
 
 module.exports = router;
