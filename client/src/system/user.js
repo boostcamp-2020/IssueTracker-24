fa322c3b764e3511e2d54d945af0d3a9893fd9fc
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-02 19:23:00 +0900

    [refactor]: 로그인 흐름의 수정으로 인한 함수 삭제 #2
    1. login 흐름을 axios가 아닌 anchor 태그로 수정
    2. githublogin 함수 삭제
    3. login 함수 (local) 삭제

--- client/src/system/user.js
+++ client/src/system/user.js
@@ -6,19 +6,6 @@ const url = {
   GITHUB_LOGIN: 'auth/github/login',
 };
 
-export const login = async (id, password) => {
-  const body = { id, password };
-
-  const response = await getData(url.LOGIN, body);
-  return response;
-};
-
-export const gitHubLogin = async () => {
-
-  const response = await getData(url.GITHUB_LOGIN);
-  return response;
-};
-
 export const logout = () => {
   removeToken();
 };
