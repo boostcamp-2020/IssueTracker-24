87e0fedf3ad5adb80e7a0cf2d29e901664756b01
JC Chung <jotace422@gmail.com>
2020-11-02 15:12:06 +0900

    [feat]: error-handler 미들웨어 추가

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 const passportConfig = require('../passport');
 const { sequelize } = require('../models');
 const authRouter = require('../routes/auth');
@@ -25,4 +26,13 @@ module.exports = (app) => {
   app.use(morgan('dev'));
   app.use(cookieParser());
   app.use('/auth', authRouter);
+
+  app.use((req, res, next) => {
+    next(createError(404));
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(err.message);
+  });
 };
