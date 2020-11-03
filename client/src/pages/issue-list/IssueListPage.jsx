53b6cae799fa75b64876cfdbf89a427993f8b31c
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-04 00:38:10 +0900

    [feature] : header 컴포넌트 생성
    1. header 컴포넌트를 생성하였습니다.

--- client/src/pages/issue-list/IssueListPage.jsx
+++ client/src/pages/issue-list/IssueListPage.jsx
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import IssuesContext from '../../context/issues-context';
 import IssueContainer from '../../components/issue/IssueContainer';
+import IssueHeader from '../../components/issue/IssueHeader';
 import reducer from './reducer';
 
 const dummyIssues = [
@@ -41,6 +42,7 @@ const IssueListPage = () => {
 
   return (
     <IssuesContext.Provider value={value}>
+      <IssueHeader/>
       <IssueContainer />
     </IssuesContext.Provider>
   );
