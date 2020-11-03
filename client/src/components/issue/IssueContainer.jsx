e4327409dae187fb09a2dd46e4a3a88f43b76445
JC Chung <jotace422@gmail.com>
2020-11-03 14:17:01 +0900

    [feat]: issue리스트와 filter를 포함하는 issueContainer 구현 #25

---
+++ client/src/components/issue/IssueContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import IssueItem from './IssueItem';
+import IssuesContext from '../../context/issues-context';
+
+const IssueContainer = () => {
+  const { issues } = useContext(IssuesContext);
+
+  return issues.map((issue) => <IssueItem key={issue.id} issue={issue} />);
+};
+
+export default IssueContainer;
