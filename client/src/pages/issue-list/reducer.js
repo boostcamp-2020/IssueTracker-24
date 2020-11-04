66d24598fde9ca36d00e4ca73a7f440cb12ff3af
JC Chung <jotace422@gmail.com>
2020-11-04 17:35:17 +0900

    [feat]: 로그인 직후 이슈 페이지에 필요한 모든 데이터 등록하는 reducer 기능 추가 #28
        issues, labels, milestones, users 데이터를 가져와 state에 등록합니다

--- client/src/pages/issue-list/reducer.js
+++ client/src/pages/issue-list/reducer.js
@@ -1,5 +1,6 @@
 export const CHECK_ISSUE = 'check issue';
 export const UNCHECK_ISSUE = 'uncheck issue';
+export const INIT_DATA = 'init data';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -16,6 +17,14 @@ const reducer = (state, action) => {
         ...state,
         checkedIssues,
       };
+    }
+    case INIT_DATA: {
+      const data = action.data;
+      const issues = [...state.issues, ...data.issues];
+      const labels = [...state.labels, ...data.labels];
+      const milestones = [...state.milestones, ...data.milestones];
+      // const users = [...state.users, ...data.users]; // TODO
+      return { ...state, issues, labels, milestones }; // TODO: add users
     }
   }
 };
