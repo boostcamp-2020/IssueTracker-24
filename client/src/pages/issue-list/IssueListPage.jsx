63ee4d05d12c523a6b15b764e12cf9b44f28ccb3
JC Chung <jotace422@gmail.com>
2020-11-04 17:35:39 +0900

    [feat]: 로그인 직후 axios를 사용하여 초기 데이터를 불러온다 #28
        useEffect를 사용하여 최초 한 번만 이슈 페이지에서 관리할 데이터들을 서버에서 받아옵니다

--- client/src/pages/issue-list/IssueListPage.jsx
+++ client/src/pages/issue-list/IssueListPage.jsx
@@ -1,52 +1,18 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import IssueContainer from '../../components/issue/IssueContainer';
 import Header from '../../components/Header';
 import reducer from './reducer';
 import MenuContainer from '../../components/issue/MenuContainer';
+import { getData } from '../../lib/request';
+import { INIT_DATA } from '../../pages/issue-list/reducer';
 
 export const IssuesContext = React.createContext();
 
-const dummyIssues = [
-  {
-    id: '1',
-    title: '리액트 환경설정',
-    labels: [
-      { id: 1, title: 'bug', color: '#f9d0c4' },
-      { id: 2, title: 'refactor', color: '#fcc7f5' },
-    ],
-  },
-  {
-    id: '2',
-    title: 'express 환경설정',
-    labels: [
-      { id: 1, title: 'bug', color: '#f9d0c4' },
-      { id: 3, title: 'Must', color: '#028e87' },
-    ],
-  },
-  {
-    id: '3',
-    title: 'webpack, babel 설정',
-    labels: [
-      { id: 1, title: 'bug', color: '#f9d0c4' },
-      { id: 2, title: 'refactor', color: '#fcc7f5' },
-    ],
-  },
-];
-
 const initialState = {
   checkedIssues: [],
-  issues: dummyIssues,
-  labels: [
-    { id: 1, title: 'bug', color: '#f9d0c4' },
-    { id: 2, title: 'refactor', color: '#fcc7f5' },
-    { id: 3, title: 'Must', color: '#028e87' },
-    { id: 4, title: 'frontend', color: '#AAED8B' },
-  ],
-  milestones: [
-    { id: 1, title: 'week-1' },
-    { id: 2, title: 'week-2' },
-    { id: 3, title: 'week-3' },
-  ],
+  issues: [],
+  labels: [],
+  milestones: [],
   users: [
     { id: 1, sns_id: 'qkrrlgh519' },
     { id: 2, sns_id: 'mu1616' },
@@ -57,6 +23,15 @@ const initialState = {
 
 const IssueListPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(async () => {
+    const issues = await getData('issues');
+    const labels = await getData('labels');
+    const milestones = await getData('milestones');
+    // TODO
+    //const users = await getData('users');
+    dispatch({ type: INIT_DATA, data: { issues, labels, milestones } }); // TODO: add users
+  }, []);
 
   return (
     <IssuesContext.Provider value={{ state, dispatch }}>
