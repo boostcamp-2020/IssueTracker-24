a8df57964146373d816e1a8dada15abac7376fe5
mu1616 <mu1616@naver.com>
2020-11-04 18:19:42 +0900

    [fix]: axios 요청 오류 수정
    토큰을 저장할 때 잘못된 값을 저장하여서 발생했던 axios 요청 오류를
    수정하였습니다.

--- client/src/utils/token.js
+++ client/src/utils/token.js
@@ -1,7 +1,7 @@
 const USER_TOKEN = 'USER_TOKEN';
 
 export const setToken = (token) => {
-  localStorage.setItem(USER_TOKEN, `bearer ${token}`);
+  localStorage.setItem(USER_TOKEN, `Bearer ${token}`);
 };
 
 export const getToken = () => localStorage.getItem(USER_TOKEN);
