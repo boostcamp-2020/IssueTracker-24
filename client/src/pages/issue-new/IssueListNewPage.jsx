4e60597ace4b36cb657ab10cff5b4434fa6ac020
JC Chung <jotace422@gmail.com>
2020-11-05 22:06:14 +0900

    [fix]: css 수정

--- client/src/pages/issue-new/IssueListNewPage.jsx
+++ client/src/pages/issue-new/IssueListNewPage.jsx
@@ -3,26 +3,35 @@ import Header from '../../components/Header';
 import styled from 'styled-components';
 import IssueNewImage from '../../components/issue/new/IssueNewImage';
 import IssueNewContent from '../../components/issue/new/IssueNewContent';
+import Sidebar from '../../components/issue/new/sidebar/Sidebar';
 const IssueListNewPageWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   margin: 0 auto;
   width: 80%;
-  height:500px;
+  height: 500px;
   margin-top: 70px;
   margin-bottom: 40px;
+  .content-wrapper {
+    display: flex;
+    flex-basis: 72%;
+    width: 70%;
+  }
 `;
 
-const IssueListNewPage = () =>{
-    return(
-      <>
-       <Header/>
-       <IssueListNewPageWrapper>
-        <IssueNewImage/>
-        <IssueNewContent/>
-    
-       </IssueListNewPageWrapper>
-     </>
-    );
-}
+const IssueListNewPage = () => {
+  return (
+    <>
+      <Header />
+      <IssueListNewPageWrapper>
+        <div class="content-wrapper">
+          <IssueNewImage />
+          <IssueNewContent />
+        </div>
+        <Sidebar />
+      </IssueListNewPageWrapper>
+    </>
+  );
+};
 
 export default IssueListNewPage;
