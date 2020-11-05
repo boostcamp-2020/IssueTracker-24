3584ddd00780b0d8d7053efceb16535ecbf1ea7e
JC Chung <jotace422@gmail.com>
2020-11-05 22:09:09 +0900

    [fix]: img의 alt 속성값 빈 문자열로 수정

--- client/src/components/issue/new/IssueNewImage.jsx
+++ client/src/components/issue/new/IssueNewImage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 const IssueNewImageWraaper = styled.div`
- .issue-profile{
-     width:30px;
-     height:30px;
-     border-radius:20px;
- }
+  .issue-profile {
+    width: 30px;
+    height: 30px;
+    border-radius: 20px;
+  }
 `;
-const IssueNewImage = () =>{
+const IssueNewImage = () => {
   return (
     <>
       <IssueNewImageWraaper>
-      <img alt="프로필사진"className="issue-profile"></img>
+        <img alt="" className="issue-profile"></img>
       </IssueNewImageWraaper>
     </>
   );
-}
+};
 
 export default IssueNewImage;
