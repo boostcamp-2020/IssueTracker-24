ea14563abcfbfde1db154b865ca789b140704ba2
mu1616 <mu1616@naver.com>
2020-11-06 08:30:23 +0900

    [style]: issueListNewPage 이름 변경
    이슈리스트생성이 아니라 이슈생성이므로 좀 더 자연스럽게 issueNewPage.jsx로 이름을 변경하였습니다.

--- client/src/pages/issue-new/IssueNewPage.jsx
+++ client/src/pages/issue-new/IssueNewPage.jsx
@@ -3,26 +3,25 @@ import Header from '../../components/Header';
 import styled from 'styled-components';
 import IssueNewImage from '../../components/issue/new/IssueNewImage';
 import IssueNewContent from '../../components/issue/new/IssueNewContent';
-const IssueListNewPageWrapper = styled.div`
+const IssueNewPageWrapper = styled.div`
   display: flex;
   margin: 0 auto;
   width: 80%;
-  height:500px;
+  height: 500px;
   margin-top: 70px;
   margin-bottom: 40px;
 `;
 
-const IssueListNewPage = () =>{
-    return(
-      <>
-       <Header/>
-       <IssueListNewPageWrapper>
-        <IssueNewImage/>
-        <IssueNewContent/>
-    
-       </IssueListNewPageWrapper>
-     </>
-    );
-}
+const IssueListNewPage = () => {
+  return (
+    <>
+      <Header />
+      <IssueNewPageWrapper>
+        <IssueNewImage />
+        <IssueNewContent />
+      </IssueNewPageWrapper>
+    </>
+  );
+};
 
 export default IssueListNewPage;
