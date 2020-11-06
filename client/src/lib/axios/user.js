206a214289ec1c47da6c273e555a99e6d85f8cd5
mu1616 <mu1616@naver.com>
2020-11-07 06:27:55 +0900

    [feat]: 전체유저 조회 axios 함수 구현
    전체 유저를 조회하는 요청을 보내는 axios 함수를 구현하였습니다.

--- client/src/lib/axios/user.js
+++ client/src/lib/axios/user.js
@@ -4,6 +4,7 @@ import { getData } from './request';
 const url = {
   LOGOUT: 'auth/logout',
   GET_CURRENT_USER: '/users/current',
+  GET_ALL_USERS: '/users',
 };
 
 export const logout = async () => {
@@ -14,4 +15,9 @@ export const logout = async () => {
 export const getCurrentUser = async () => {
   const currentUser = await getData(url.GET_CURRENT_USER);
   return currentUser;
+};
+
+export const getAllUsers = async () => {
+  const users = await getData(url.GET_ALL_USERS);
+  return users;
 };
