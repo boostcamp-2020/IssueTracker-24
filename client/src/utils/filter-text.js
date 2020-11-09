b1ac67bd362f05b030b04a8bc06fe1a1a6a74d18
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-09 17:04:23 +0900

    [feat]: 검색창 text를 open/closed로 filter 함수 구현 # 71
    1. open으로 filter 함수 구현
    2. closed로 filter 함수 구현

--- client/src/utils/filter-text.js
+++ client/src/utils/filter-text.js
@@ -14,3 +14,41 @@ export const filterTextByYourIssues = (searchText) => {
     return [...sliceLast(splitedText), 'author:@me', ''].join(' ');
   }
 };
+
+export const filterTextByOpen = (searchText) => {
+  const splitedText = searchText.split(' ');
+  let index = -1;
+  splitedText.forEach((v, i) => {
+    if (v.indexOf('is:open') !== -1) {
+      index = i;
+    } else if (v.indexOf('is:closed') !== -1) {
+      index = i;
+    }
+  });
+
+  if (index !== -1) {
+    splitedText[index] = 'is:open';
+    return [...splitedText].join(' ');
+  } else {
+    return [...sliceLast(splitedText), 'is:open', ''].join(' ');
+  }
+};
+
+export const filterTextByClosed = (searchText) => {
+  const splitedText = searchText.split(' ');
+  let index = -1;
+  splitedText.forEach((v, i) => {
+    if (v.indexOf('is:open') !== -1) {
+      index = i;
+    } else if (v.indexOf('is:closed') !== -1) {
+      index = i;
+    }
+  });
+
+  if (index !== -1) {
+    splitedText[index] = 'is:closed';
+    return [...splitedText].join(' ');
+  } else {
+    return [...sliceLast(splitedText), 'is:closed', ''].join(' ');
+  }
+};
