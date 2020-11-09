a3215a018da5b84e3f44f2a941f50d08208740b7
mu1616 <mu1616@naver.com>
2020-11-10 08:32:04 +0900

    [feat]: 마일스톤 편집 페이지 라우팅 설정
    마일스톤 편집 페이지 라우팅 설정을 하였습니다.
    match.params로 마일스톤 id를 받아오도록 구현하였습니다.

---
+++ client/src/pages/MilestoneEditPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MilestoneEditPage = ({ match }) => {
+  console.log(match.params.id);
+  return <></>;
+};
+
+export default MilestoneEditPage;
