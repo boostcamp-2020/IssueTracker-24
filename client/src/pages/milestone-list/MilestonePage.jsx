6b8eef3e0cfd9d93c3ae52fdb1ef0130c4bc867f
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-09 15:58:15 +0900

    [feature] : 마일스톤. ui
    1. open, closed 모양 표시하였습니다.

--- client/src/pages/milestone-list/MilestonePage.jsx
+++ client/src/pages/milestone-list/MilestonePage.jsx
@@ -28,6 +28,27 @@ const CreateMilestone = styled.button`
   border-radius:5px;
 `;
 
+const MilestoneNav = styled.div`
+   display:flex;
+   width:80%;
+   height:50px;
+   margin:40px auto;
+   background-color:#F6F8F4;
+   border-top-left-radius:5px;
+   border-top-right-radius:5px;
+   border:1px solid #E1E4E8;
+   padding:10px 10px;
+   padding-left:10px;
+   box-sizing : border-box;
+`;
+const OpenDiv = styled.div`
+ margin-left:5px;
+ padding-left:10px;
+`
+const CloseDiv = styled.div`
+ margin-left:5px;
+ padding-left:10px;
+`;
 const MilestonePage = () =>{
     return (
       <>
@@ -41,7 +62,14 @@ const MilestonePage = () =>{
              New Milestones
          </CreateMilestone>
        </MilestoneHeader>
- 
+       <MilestoneNav>
+         <OpenDiv>
+           {svg['Milestones']} 1 Open
+         </OpenDiv>
+         <CloseDiv>
+           {svg['closeMilestones']} 0 Close
+         </CloseDiv>
+       </MilestoneNav>
       </>
     );
 }
