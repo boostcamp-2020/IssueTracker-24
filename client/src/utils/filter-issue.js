ee36294238334341cd6c98b6994079b023e0f74f
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-10 20:42:43 +0900

    [feat]: issues를 assignee에 따라 filter 함수 구현 #71

--- client/src/utils/filter-issue.js
+++ client/src/utils/filter-issue.js
@@ -1,4 +1,5 @@
 const AUTHOR_POSITION = 1;
+const ASSIGNEE_POSITION = 1;
 
 const filterOpenClose = (issues, searchText) => {
   if (searchText.indexOf('is:open') !== -1) {
@@ -24,6 +25,37 @@ const filterAuthor = (issues, searchText, currentUser) => {
     author = author.split(':')[AUTHOR_POSITION];
 
     return issues.filter((issue) => issue.user.sns_id === author);
+  }
+
+  return issues;
+};
+
+const filterAssignee = (issues, searchText, currentUser) => {
+  if (searchText.indexOf('no:assignee') !== -1) {
+    return issues.filter((issue) => issue.assignees.length === 0);
+  }
+
+  if (searchText.indexOf('assignee:@me') !== -1) {
+    return issues.filter((issue) =>
+      issue.assignees.some(
+        (assignee) => assignee.sns_id === currentUser.sns_id,
+      ),
+    );
+  }
+
+  if (searchText.indexOf('assignee:') !== -1) {
+    const splitedText = searchText.split(' ');
+    let assignee = '';
+    splitedText.forEach((v) => {
+      if (v.indexOf('assignee:') !== -1) assignee = v;
+    });
+    assignee = assignee.split(':')[ASSIGNEE_POSITION];
+
+    return issues.filter((issue) =>
+      issue.assignees.some(
+        (issueAssignee) => issueAssignee.sns_id === assignee,
+      ),
+    );
   }
 
   return issues;
