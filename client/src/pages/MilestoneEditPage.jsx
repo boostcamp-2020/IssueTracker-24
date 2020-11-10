c18cef4b8f04532cd8271de827d6ff5870cdfb7c
mu1616 <mu1616@naver.com>
2020-11-10 13:30:19 +0900

    [feat]: 마일스톤 편집 페이지 헤더부분 UI 구현
    마일스톤 편집 페이지의 헤더와 selectMenu 부분 UI를 구현하였습니다.

--- client/src/pages/MilestoneEditPage.jsx
+++ client/src/pages/MilestoneEditPage.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { getMilestone } from '../lib/axios/milestone';
+import { useHistory } from 'react-router-dom';
 import MilestoneEditForm from '../components/milestone/MilestoneEditForm';
+import LabelComponent from '../components/common/LabelComponent';
+import svg from '../utils/svg';
+import Header from '../components/Header';
 
 const MilestoneNewPageWrappper = styled.div`
   display: flex;
@@ -9,10 +13,37 @@ const MilestoneNewPageWrappper = styled.div`
   margin: 0 auto;
   flex-direction: column;
 `;
+
+const SelectMenuWrapper = styled.div`
+  display: flex;
+  height: 40px;
+  border-bottom: 1px solid black;
+  margin-top: 15px;
+  margin-bottom: 13px;
+  padding-bottom: 10px;
+`;
+
 const MilestoneEditPage = ({ match }) => {
+  const history = useHistory();
+  const onClickMilestoneMenu = () => history.push('/milestones');
+  const onClickLabelMenu = () => history.push('/labels');
+
   return (
     <>
+      <Header />
       <MilestoneNewPageWrappper>
+        <SelectMenuWrapper>
+          <LabelComponent
+            svg={svg['Labels']}
+            title="Labels"
+            func={onClickLabelMenu}
+          />
+          <LabelComponent
+            svg={svg['Milestones']}
+            title="Milestones"
+            func={onClickMilestoneMenu}
+          />
+        </SelectMenuWrapper>
         <MilestoneEditForm />
       </MilestoneNewPageWrappper>
     </>
