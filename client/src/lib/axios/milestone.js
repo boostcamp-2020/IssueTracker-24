a474c63b7e8e47316cf8a08b8a8f256862b4669f
mu1616 <mu1616@naver.com>
2020-11-10 12:19:37 +0900

    [feat]: 마일스톤 조회 axios 함수 구현
    특정 마일스톤 조회 요청을 보내는 axios 함수를 구현하였습니다.

--- client/src/lib/axios/milestone.js
+++ client/src/lib/axios/milestone.js
@@ -3,6 +3,7 @@ import { getData, postData } from './request';
 const url = {
   GET_ALL_MILESTONES: 'milestones',
   CREATE_MILESTONE: 'milestones',
+  GET_MILESTONE: 'milestones/',
 };
 
 export const getAllMilestones = async () => {
@@ -12,5 +13,10 @@ export const getAllMilestones = async () => {
 
 export const createMilestone = async (body) => {
   const milestone = await postData(url.CREATE_MILESTONE, body);
+  return milestone;
+};
+
+export const getMilestone = async (id) => {
+  const milestone = await getData(`${url.GET_MILESTONE}${id}`);
   return milestone;
 };
