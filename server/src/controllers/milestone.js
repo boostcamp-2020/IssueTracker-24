418d30ce18117f5ec1ebb811688ba9c38ae237e0
mu1616 <mu1616@naver.com>
2020-11-10 09:05:33 +0900

    [feat]: 마일스톤 수정(put) API 구현
    특정 마일스톤 정보를 수정(PUT) 할 수 있는 백엔드 API를 구현하였습니다.

--- server/src/controllers/milestone.js
+++ server/src/controllers/milestone.js
@@ -30,4 +30,21 @@ const getMilestone = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllMilestone, createMilestone, getMilestone };
+const updateMilestone = async (req, res, next) => {
+  const { id } = req.params;
+  const { title, due_date, description, state } = req.body;
+  console.log('update!');
+  await Milestone.update(
+    {
+      title,
+      description,
+      due_date: due_date ? new Date(due_date) : null,
+      state,
+    },
+    { where: { id } },
+  );
+  const milestone = await Milestone.findByPk(id, { attributes: ['id', 'title', 'description', 'due_date', 'state'] });
+  return res.status(200).json(milestone);
+};
+
+module.exports = { getAllMilestone, createMilestone, getMilestone, updateMilestone };
