a45c6ff2a222a1fc54ac6f6ec8f6c358b888e27c
JC Chung <jotace422@gmail.com>
2020-11-12 04:08:32 +0900

    [fix] 스페이스/줄간격 변화?

--- client/src/components/milestone/MilestoneSelectMenu.jsx
+++ client/src/components/milestone/MilestoneSelectMenu.jsx
@@ -3,20 +3,17 @@ import styled from 'styled-components';
 import LabelComponent from '../common/LabelComponent';
 import svg from '../../utils/svg';
 
-
 const MilestoneSelectMenuWrapper = styled.div`
-  display:flex;
+  display: flex;
 `;
 
-
-const MilestoneSelectMenu = () =>{
-  
-    return (
-        <MilestoneSelectMenuWrapper>
-           <LabelComponent svg={svg['Labels']} title='Labels'/>
-           <LabelComponent svg={svg['Milestones']} title='Milestones'/>
-        </MilestoneSelectMenuWrapper>
-    )
-}
+const MilestoneSelectMenu = () => {
+  return (
+    <MilestoneSelectMenuWrapper>
+      <LabelComponent svg={svg['Labels']} title="Labelssss" />
+      <LabelComponent svg={svg['Milestones']} title="Milestones" />
+    </MilestoneSelectMenuWrapper>
+  );
+};
 
 export default MilestoneSelectMenu;
