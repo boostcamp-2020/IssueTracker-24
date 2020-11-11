f6d4b5fd2c1dce9155a07a7c4befc947b1cb307d
JC Chung <jotace422@gmail.com>
2020-11-12 04:06:32 +0900

    [refactor]: Navigation 컴포넌트를 재활용하도록 수정

--- client/src/pages/label-list/LabelListPage.jsx
+++ client/src/pages/label-list/LabelListPage.jsx
@@ -4,8 +4,16 @@ import { getAllIssues } from '../../lib/axios/issue';
 import { getAllLabels } from '../../lib/axios/label';
 import Header from '../../components/Header';
 import LabelListContainer from '../../components/label/LabelListContainer';
-import Navigation from '../../components/label/Navigation';
+import Navigation from '../../components/common/Navigation';
+import GreenButton from '../../components/common/GreenButton';
 import PageBody from '../../components/label/PageBody';
+import styled from 'styled-components';
+
+const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
 
 export const LabelsContext = React.createContext();
 
@@ -32,7 +40,10 @@ const LabelListPage = () => {
     <LabelsContext.Provider value={{ state, dispatch }}>
       <Header />
       <PageBody>
-        <Navigation />
+        <NavigationWrapper>
+          <Navigation cur={'labels'} />
+          <GreenButton text={'New label'} />
+        </NavigationWrapper>
         <LabelListContainer />
       </PageBody>
     </LabelsContext.Provider>
