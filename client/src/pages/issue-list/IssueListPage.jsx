9c5d441fae2a4770ca4fe4f536d380a9b8df82d5
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-11 15:05:42 +0900

    [fix]: rebase 도중 발생한 충돌 해결

--- client/src/pages/issue-list/IssueListPage.jsx
+++ client/src/pages/issue-list/IssueListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState, useContext } from 'react';
 import IssueContainer from '../../components/issue/IssueContainer';
 import Header from '../../components/Header';
 import reducer from './reducer';
@@ -6,10 +6,11 @@ import MenuContainer from '../../components/issue/MenuContainer';
 import { getAllIssues } from '../../lib/axios/issue';
 import { getAllLabels } from '../../lib/axios/label';
 import { getAllMilestones } from '../../lib/axios/milestone';
-import { getCurrentUser, getAllUsers } from '../../lib/axios/user';
+import { getAllUsers } from '../../lib/axios/user';
 import { INIT_DATA } from '../../pages/issue-list/reducer';
 import ToolBarContainer from '../../components/issue/ToolBarContainer';
 import Spinner from '../../components/common/Spinner';
+import { AppContext } from '../../App';
 
 export const IssuesContext = React.createContext();
 
@@ -25,21 +26,22 @@ const initialState = {
 };
 
 const IssueListPage = () => {
-  const [isCompleteRequest, setIsCompleteRequest] = useState(false);
+  const { currentUser } = useContext(AppContext);
+  initialState.currentUser = currentUser;
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [isCompleteRequest, setIsCompleteRequest] = useState(false);
 
   useEffect(async () => {
-    const [issues, labels, milestones, users, currentUser] = await Promise.all([
+    const [issues, labels, milestones, users] = await Promise.all([
       getAllIssues(),
       getAllLabels(),
       getAllMilestones(),
       getAllUsers(),
-      getCurrentUser(),
     ]);
     setIsCompleteRequest(true);
     dispatch({
       type: INIT_DATA,
-      data: { issues, labels, milestones, users, currentUser },
+      data: { issues, labels, milestones, users },
     });
   }, []);
 
