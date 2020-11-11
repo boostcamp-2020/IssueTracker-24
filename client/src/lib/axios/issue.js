a9246e4388f5317551b91611fdd647e9c78e2ea3
mu1616 <mu1616@naver.com>
2020-11-11 12:09:01 +0900

    [feat]: 이슈 patch axios 함수 구현
    이슈의 정보를 일부 수정하는 patch 요청을 보내는 axios 함수를 구현하였습니다.

--- client/src/lib/axios/issue.js
+++ client/src/lib/axios/issue.js
@@ -3,6 +3,7 @@ import { getData } from './request';
 const url = {
   GET_ALL_ISSUES: 'issues',
   GET_ISSUES: 'issues/',
+  PATCH_ISSUE: 'issues/',
 };
 
 export const getAllIssues = async () => {
@@ -12,5 +13,10 @@ export const getAllIssues = async () => {
 
 export const getIssue = async (id) => {
   const issue = await getData(`${url.GET_ISSUES}${id}`);
+  return issue;
+};
+
+export const patchIssue = async (id, body) => {
+  const issue = await patchData(`${url.PATCH_ISSUE}${id}`, body);
   return issue;
 };
