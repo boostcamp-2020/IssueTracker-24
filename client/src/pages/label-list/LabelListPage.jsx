13d39ccbdb227fe732373a47a69d76b7a931c0d2
JC Chung <jotace422@gmail.com>
2020-11-11 19:06:06 +0900

    [refactor]: 불필요한 코드 삭제

--- client/src/pages/label-list/LabelListPage.jsx
+++ client/src/pages/label-list/LabelListPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useReducer } from 'react';
 import reducer, { INIT_DATA } from './reducer';
 import { getAllIssues } from '../../lib/axios/issue';
 import { getAllLabels } from '../../lib/axios/label';
-import { getAllMilestones } from '../../lib/axios/milestone';
 import Header from '../../components/Header';
 import LabelListContainer from '../../components/label/LabelListContainer';
 import Navigation from '../../components/label/Navigation';
@@ -13,21 +12,19 @@ export const LabelsContext = React.createContext();
 const initialState = {
   issues: [],
   labels: [],
-  milestones: [],
 };
 
 const LabelListPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(async () => {
-    const [issues, labels, milestones] = await Promise.all([
+    const [issues, labels] = await Promise.all([
       getAllIssues(),
       getAllLabels(),
-      getAllMilestones(),
     ]);
     dispatch({
       type: INIT_DATA,
-      data: { issues, labels, milestones },
+      data: { issues, labels },
     });
   }, []);
 
