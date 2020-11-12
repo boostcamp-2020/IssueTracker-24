8bd82e6d10cc5404c3eb580c9e883b08703ddffd
mu1616 <mu1616@naver.com>
2020-11-12 14:07:28 +0900

    [fix]: milestoneList 페이지 오류 수정
    마일스톤을 open/reopen 할 때 이슈목록이 출력되는 오류를 수정하였습니다.
    CAHGNE_STATE ACTION을 reducer에 추가하였습니다.

--- client/src/pages/milestone-list/reducer.js
+++ client/src/pages/milestone-list/reducer.js
@@ -1,7 +1,11 @@
+import milestone from '../../../../server/src/models/milestone';
+
 export const INIT_DATA = 'init data';
 export const ROUTING_LABEL = 'routing label';
 export const FILTERING_MILESTONE = 'filtering milestone';
 export const DELETE_MILESTONE = 'delete milestone';
+export const CHANGE_STATE = 'change state';
+
 const reducer = (state, action) => {
   switch (action.type) {
     case INIT_DATA: {
@@ -32,6 +36,20 @@ const reducer = (state, action) => {
         ...state,
         display: action.display,
         milestoneId: action.milestoneId,
+      };
+    }
+    case CHANGE_STATE: {
+      const milestones = state.milestones.map((milestone) => {
+        if (milestone.id != action.patchedMilestone.id) return milestone;
+        else return action.patchedMilestone;
+      });
+      const milestoneList = milestones.filter(
+        (milestone) => milestone.state != action.patchedMilestone.state,
+      );
+      return {
+        ...state,
+        milestoneList,
+        milestones,
       };
     }
   }
