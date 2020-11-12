68f04c8fe910caf16945aaac966f7411a73aa861
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-13 01:58:57 +0900

    [refactor]: 저자 메뉴 드롭다운 컴포넌트 리팩토링

--- client/src/components/issue/toolbar/AuthorMenuDropDown.jsx
+++ client/src/components/issue/toolbar/AuthorMenuDropDown.jsx
@@ -1,46 +1,12 @@
 import React, { useContext } from 'react';
-import styled from 'styled-components';
 import { IssuesContext } from '../../../pages/issue-list/IssueListPage';
 import ProfileImage from '../../common/ProfileImage';
 import { FILTER_ISSUES_BY_AUTHOR } from '../../../pages/issue-list/reducer';
+import DetailsMenuDropDown from './common/DetailsMenuDropDown';
+import DetailsItem from './common/DetailsItem';
 
-const DetailsItem = styled.div`
-  display: flex;
-  border-bottom: 1px solid #eaecef;
-  height: 32px;
-  box-sizing: border-box;
-  padding: 5px;
-  padding-left: 20px;
-  font-size: 13px;
-  cursor: pointer;
-  &:hover {
-    background-color: #e9e9e9;
-  }
-  &:nth-child(1) {
-    font-weight: 600;
-    background-color: #fafbfc;
-    cursor: auto;
-    &:hover {
-      background-color: #fafbfc;
-    }
-  }
-  .user-id {
-    margin-left: 10px;
-  }
-`;
-
-const DetailsMenuDropDown = styled.div`
-  width: 250px;
-  position: absolute;
-  top: 70px;
-  left: -70px;
-  z-index: 10;
-  border: 1px solid #eaecef;
-  margin-top: -30px;
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  border-radius: 4px;
+const IdWrapper = styled.div`
+  margin-left: 10px;
 `;
 
 const AuthorMenuDropDown = ({ setShowAuthorMenu }) => {
@@ -63,17 +29,17 @@ const AuthorMenuDropDown = ({ setShowAuthorMenu }) => {
 
   return (
     <>
-      <DetailsMenuDropDown>
+      <DetailsMenuDropDown left={'-70px'}>
         <DetailsItem onClick={onClickFirstItem}>Filter by author</DetailsItem>
         {users.map((user, index) => (
           <DetailsItem
             className={'author-item'}
             key={index}
             onClick={onClickDetailsItem}
-            data-name={user.sns_id}
+            data={user.sns_id}
           >
             <ProfileImage image={user.profile_image} size={20} />
-            <div className="user-id">{user.sns_id}</div>
+            <IdWrapper>{user.sns_id}</IdWrapper>
           </DetailsItem>
         ))}
       </DetailsMenuDropDown>
