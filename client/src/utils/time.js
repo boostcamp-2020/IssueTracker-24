7b44bb3bb71c543ae6bbfbcab67a0467d650501e
thdwlsgus0 <thdwlsgus0@naver.com>
2020-11-12 22:52:25 +0900

    [fix] : 재수정
    1. 일+1->월+1로 수정하였습니다.

--- client/src/utils/time.js
+++ client/src/utils/time.js
@@ -51,8 +51,8 @@ export const getDueInfo = (time) => {
 export const getFormattedDate = ({ date, format }) => {
   if (format === '-') {
     const year = date.getFullYear();
-    const month = String(date.getMonth()).padStart(2, 0);
-    const day = String(date.getDate()+1).padStart(2, 0);
+    const month = String(date.getMonth()+1).padStart(2, 0);
+    const day = String(date.getDate()).padStart(2, 0);
     return `${year}-${month}-${day}`;
   }
 };
