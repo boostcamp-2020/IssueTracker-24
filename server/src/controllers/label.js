a9173cfcf279391f3f75ebf94d31843354017856
JC Chung <jotace422@gmail.com>
2020-11-12 18:07:25 +0900

    [feat]: 라벨 삭제 api 구현

--- server/src/controllers/label.js
+++ server/src/controllers/label.js
@@ -4,4 +4,17 @@ const getAllLabels = async (req, res, next) => {
   const lables = await Label.findAll({});
   res.status(200).json(lables);
 };
-module.exports = { getAllLabels };
+
+const deleteLabel = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const label = await Label.destroy({
+      where: { id },
+    });
+    return res.status(201).json(label);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getAllLabels, deleteLabel };
