52fe615c25c908fce6e4c601cc734fb17a1fb1dc
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-12 14:57:14 +0900

    [feat]: label 생성 api 구현 #88

--- server/src/controllers/label.js
+++ server/src/controllers/label.js
@@ -4,4 +4,18 @@ const getAllLabels = async (req, res, next) => {
   const lables = await Label.findAll({});
   res.status(200).json(lables);
 };
-module.exports = { getAllLabels };
+
+const createLabel = async (req, res, next) => {
+  const { title, description, color } = { ...req.body };
+  try {
+    const label = await Label.create({
+      title,
+      description,
+      color,
+    });
+    res.status(200).json(label);
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports = { getAllLabels, createLabel };
--- server/src/routes/labels.js
+++ server/src/routes/labels.js
@@ -5,4 +5,5 @@ const labelController = require('../controllers/label');
 const { isAuth } = require('../middlewares/auth');
 
 router.get('/', isAuth, labelController.getAllLabels);
+router.post('/', isAuth, labelController.createLabel);
 module.exports = router;
