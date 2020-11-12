d42ecd36082d0dc4624615eda0e70479080c9a2a
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-12 14:59:48 +0900

    [feat]: label 생성후 페이지의 label 목록을 갱신하는 action 추가 #84

--- client/src/pages/label-list/reducer.js
+++ client/src/pages/label-list/reducer.js
@@ -1,8 +1,16 @@
 export const INIT_DATA = 'init data';
+export const RENEW_LABEL = 'renew label';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case INIT_DATA: {
+      return {
+        ...state,
+        ...action.data,
+      };
+    }
+    case RENEW_LABEL: {
+      console.log(action.data);
       return {
         ...state,
         ...action.data,
