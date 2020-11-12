26fcc06b26ab31e429352ca086608a11a7ea8ecb
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-13 00:26:59 +0900

    [refactor]: common 컴포넌트 lint & prettier 적용 및 log 삭제

--- client/src/components/common/sidebar/Label.jsx
+++ client/src/components/common/sidebar/Label.jsx
@@ -1,27 +1,27 @@
-import React, {useState, useContext, useEffect} from 'react';
-import styled from 'styled-components'; 
+import React, { useState, useContext, useEffect } from 'react';
+import styled from 'styled-components';
 import SmallLabel from '../SmallLabel';
 import svg from '../../../utils/svg';
-import {addLabel, removeLabel} from '../../../lib/axios/issue';
-import {IssueContext} from '../../../pages/IssueDetailPage';
+import { addLabel, removeLabel } from '../../../lib/axios/issue';
+import { IssueContext } from '../../../pages/IssueDetailPage';
 const LabelWrapper = styled.div`
   border-bottom: 1px solid #eaecef;
-  cursor:pointer;
-  &:hover{
-    background-color:#0366D6;
-    color:#fff;
- }
- .display-visible {
-  visibility: visible;
-}
-.display-hidden {
-  visibility: hidden;
-}
+  cursor: pointer;
+  &:hover {
+    background-color: #0366d6;
+    color: #fff;
+  }
+  .display-visible {
+    visibility: visible;
+  }
+  .display-hidden {
+    visibility: hidden;
+  }
 `;
 const LabelOptionContent = styled.div`
   flex-basis: 80%;
   display: flex;
-  justify-content:space-between;
+  justify-content: space-between;
   width: 100%;
 `;
 const CheckWrapper = styled.div`
@@ -30,46 +30,47 @@ const CheckWrapper = styled.div`
 `;
 const LabelOptionTitle = styled.div``;
 const LabelOptionDescription = styled.div`
-  display:flex;
-  justify-content:center;
+  display: flex;
+  justify-content: center;
 `;
-const CancelWrapper = styled.div`
-`;
+const CancelWrapper = styled.div``;
 const LabelHeader = styled.div`
-  display:flex;
+  display: flex;
 `;
-const Label = ({id, color, title, description}) =>{
-  const {issue, setIssue} = useContext(IssueContext);
+const Label = ({ id, color, title, description }) => {
+  const { issue, setIssue } = useContext(IssueContext);
   const [checked, setCheck] = useState(false);
   const checkDisplay = checked ? 'display-visible' : 'display-hidden';
-  const handleOnClick = async () =>{
+  const handleOnClick = async () => {
     let patchedLabel;
-    if(!checked){
-       patchedLabel = await addLabel(issue.id, id);
-    }else{
-       patchedLabel = await removeLabel(issue.id, id);
+    if (!checked) {
+      patchedLabel = await addLabel(issue.id, id);
+    } else {
+      patchedLabel = await removeLabel(issue.id, id);
     }
-  
+
     setIssue(patchedLabel);
     setCheck(!checked);
-  }
-  useEffect(()=>{
-    if(issue.labels.some((label)=>label.id===id)){
-       setCheck(true);
+  };
+  useEffect(() => {
+    if (issue.labels.some((label) => label.id === id)) {
+      setCheck(true);
     }
- }, []);
+  }, []);
   return (
-        <LabelWrapper onClick={handleOnClick}>
-          <LabelOptionContent>
-            <CheckWrapper className={checkDisplay}>{svg.checkIcon}</CheckWrapper>
-            <LabelHeader>
-              <SmallLabel color={color} size={14} marginTop={2} ></SmallLabel>
-              <LabelOptionTitle>{title}</LabelOptionTitle>
-            </LabelHeader>  
-             <CancelWrapper className={checkDisplay}>{svg.cancelButton}</CancelWrapper>
-          </LabelOptionContent> 
-          <LabelOptionDescription>{description}</LabelOptionDescription>
-        </LabelWrapper>
+    <LabelWrapper onClick={handleOnClick}>
+      <LabelOptionContent>
+        <CheckWrapper className={checkDisplay}>{svg.checkIcon}</CheckWrapper>
+        <LabelHeader>
+          <SmallLabel color={color} size={14} marginTop={2}></SmallLabel>
+          <LabelOptionTitle>{title}</LabelOptionTitle>
+        </LabelHeader>
+        <CancelWrapper className={checkDisplay}>
+          {svg.cancelButton}
+        </CancelWrapper>
+      </LabelOptionContent>
+      <LabelOptionDescription>{description}</LabelOptionDescription>
+    </LabelWrapper>
   );
-}
+};
 export default Label;
