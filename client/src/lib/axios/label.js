16716ff72c9542566bcb868233c81f2a8df3daf0
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-12 17:33:00 +0900

    [feat]: front 쪽에서 label patch axios 로직 구현

--- client/src/lib/axios/label.js
+++ client/src/lib/axios/label.js
@@ -1,8 +1,9 @@
-import { getData, postData } from './request';
+import { getData, postData, patchData } from './request';
 
 const url = {
   GET_ALL_LABELS: 'labels',
   POST_LABEL: 'labels',
+  PATCH_LABEL: 'labels/',
 };
 
 export const getAllLabels = async () => {
@@ -12,5 +13,10 @@ export const getAllLabels = async () => {
 
 export const createLabel = async (body) => {
   const label = await postData(url.POST_LABEL, body);
+  return label;
+};
+
+export const patchLabel = async (id, body) => {
+  const label = await patchData(`${url.PATCH_LABEL}${id}`, body);
   return label;
 };
