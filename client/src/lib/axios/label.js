927ff653d5732dd7ffb1c75c1df7fa1ab9b4bbe5
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-12 14:58:59 +0900

    [feat]: front쪽에서 label 생성 api 관련 로직 구현 #88

--- client/src/lib/axios/label.js
+++ client/src/lib/axios/label.js
@@ -1,10 +1,16 @@
-import { getData } from './request';
+import { getData, postData } from './request';
 
 const url = {
   GET_ALL_LABELS: 'labels',
+  POST_LABEL: 'labels',
 };
 
 export const getAllLabels = async () => {
   const labels = await getData(url.GET_ALL_LABELS);
   return labels;
+};
+
+export const createLabel = async (body) => {
+  const label = await postData(url.POST_LABEL, body);
+  return label;
 };
