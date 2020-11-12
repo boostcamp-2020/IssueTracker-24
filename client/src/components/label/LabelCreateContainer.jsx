d183e3852a8dc0b6844dcad64841872847da7c6a
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-12 17:22:55 +0900

    [refactor]: 라벨 생성 컴포넌트 리팩토링

--- client/src/components/label/LabelCreateContainer.jsx
+++ client/src/components/label/LabelCreateContainer.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from 'react';
-import styled from 'styled-components';
 import SelectedLabel from '../common/SelectedLabel';
 import GreenButton from '../common/GreenButton';
 import GreyButton from '../common/GreyButton';
@@ -12,30 +11,7 @@ import ButtonContent from './common/ButtonContent';
 import { getAllLabels, createLabel } from '../../lib/axios/label';
 import { LabelsContext } from '../../pages/label-list/LabelListPage';
 import { RENEW_LABEL } from '../../pages/label-list/reducer';
-
-const ContentWrapper = styled.div`
-  display: flex;
-  font-size: 14px;
-  margin: 25px 0 15px 0;
-
-  label {
-    font-weight: 600;
-    margin-bottom: 7px;
-    color: #24292e;
-  }
-
-  input {
-    height: 25px;
-    padding: 2px 5px;
-    outline: 0;
-    border: 1px solid #e1e4e8;
-    border-radius: 5px;
-    &:focus {
-      border: 0.5px solid #0366d6;
-      box-shadow: 0px 0px 2.5px 2.5px #b3d1f3;
-    }
-  }
-`;
+import ContentWrapper from './common/ContentWrapper';
 
 const getTitle = (title) => (title.length > 0 ? title : 'Label preview');
 
@@ -46,6 +22,7 @@ const LabelCreateContainer = ({ setCreate }) => {
   const [color, setColor] = useState(getRandomColor());
 
   const onClickCancel = (e) => setCreate(false);
+
   const onClickCreate = async (e) => {
     const body = { title, description, color };
     const label = await createLabel(body);
@@ -54,10 +31,11 @@ const LabelCreateContainer = ({ setCreate }) => {
     dispatch({ type: RENEW_LABEL, data: { labels } });
     setCreate(false);
   };
+
   const isDisabled = (title) => title.length <= 0;
 
   return (
-    <WriteContainer>
+    <WriteContainer edit={false}>
       <div>
         <SelectedLabel label={{ title: getTitle(title), color }} />
       </div>
