77f5efbcb40f66cd9385c3f1177af12fabb682ab
qkrrlgh519 <qkrrlgh519@naver.com>
2020-11-13 00:35:27 +0900

    [refactor]: filter 관련 사용하지 않는 모듈 삭제

--- client/src/components/issue/filter/FilterMenuDropDown.jsx
+++ client/src/components/issue/filter/FilterMenuDropDown.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext, useRef } from 'react';
 import styled from 'styled-components';
 import { IssuesContext } from '../../../pages/issue-list/IssueListPage';
 import {
